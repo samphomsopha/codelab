d25048113fca1bf9c45246ac23caf889b2920c95
Sam Phomsopha <samphomsopha@gmail.com>
2015-11-23 13:16:09 -0800

    revert previous commit

--- app/Http/Controllers/ChatServiceController.php
+++ app/Http/Controllers/ChatServiceController.php
@@ -97,7 +97,6 @@ class ChatServiceController extends Controller {
             $chatRoomId = $request->input('chat_id');
             // save file to Parse
             try {
-                var_dump($_FILES);die();
                 $file = ParseFile::createFromData( file_get_contents( $_FILES['image']['tmp_name'] ), $_FILES['image']['name']  );
                 $file->save();
 

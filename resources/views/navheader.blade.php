3d95a47e5fd3384fcb6cd7353296813c4b6c280b
Sam Phomsopha <samphomsopha@gmail.com>
2015-12-06 23:48:39 -0800

    update logo

--- resources/views/navheader.blade.php
+++ resources/views/navheader.blade.php
@@ -1,11 +1,13 @@
 <div class="row card">
     <div class="col-xs-12 text-center col-md-6 col-md-offset-3">
         <div class="row">
-            <div class="col-xs-2"><a href="#"><i class="fa fa-chevron-left nav-left"></i></a></div>
+            <div class="col-xs-1"><a href="#"><i class="fa fa-chevron-left nav-left"></i></a></div>
             <div class="col-xs-8">
                 <h1 class="nav-title">{{$navTitle}}</h1>
             </div>
-            <div class="col-xs-2"><a href="/logout">Logout</a></div>
+            <div class="col-xs-3">
+                <img class="logo" src="/img/logo.png"/>
+            </div>
         </div>
     </div>
 </div>
--- resources/views/notification.blade.php
+++ resources/views/notification.blade.php
@@ -30,10 +30,10 @@
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-xs-12">
-                <div class="spacer"></div>
-            </div>
-        </div>
     @endforeach
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="spacer"></div>
+        </div>
+    </div>
 @stop

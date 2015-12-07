cec477044ed731fe9d397c90c4dac176062e2a4c
Sam Phomsopha <samphomsopha@gmail.com>
2015-12-07 13:16:24 -0800

    update month calendar heading

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -16,13 +16,13 @@
                     <a class="pull-right" data-go="next"><div class="btn btn-primary">Next</div></a>
                 </div><hr/>
                 <div class="day-headers">
+                    <div class="day header">Sun</div>
                     <div class="day header">Mon</div>
                     <div class="day header">Tue</div>
                     <div class="day header">Wed</div>
                     <div class="day header">Thu</div>
                     <div class="day header">Fri</div>
                     <div class="day header">Sat</div>
-                    <div class="day header">Sun</div>
                 </div>
                 <div class="days" data-group="days">
                     <!-- the place where days will be generated -->
